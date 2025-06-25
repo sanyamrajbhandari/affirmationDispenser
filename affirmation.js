a990d19ef553a00a7d04215fa3c077e63f26e797
Sanyam Rajbhandari <sanyamrajbhandari7@gmail.com>
2025-06-25 13:56:26 +0545

    Finished everything

--- affirmation.js
+++ affirmation.js
@@ -1,15 +1,24 @@
+const affirmation = document.getElementById("quote");
+const button = document.getElementById("getMore");
 const  getAffirmation = async ()=>{
     try{
-        response = await fetch("https://www.affirmations.dev/")
+        response = await fetch(`https://api.allorigins.win/raw?url=https://www.affirmations.dev/?t=${Date.now()}`)
 
         if(!response.ok){
             throw new Error(`HTTP error! Status: ${response.status}`)
         }
         data = await response.json();
         console.log(data)
+        affirmation.textContent = data.affirmation;
     }catch(error){
         console.log("An errror occured!:",error.message)
     }
 }
 
 getAffirmation();
+button.addEventListener("click",()=>{
+    getAffirmation();
+    console.log("Button clicked!")
+});
+
+
